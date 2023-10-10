10e6d13879925970a5fae3425e27ee0166b65557
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-10-10 18:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,23 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
-};
+	// Initialize a variable to count the number of books read
+  let count = 0;
 
+  // Iterate through the books in the library
+  for (let i = 0; i < library.length; i++) {
+    // Check if the readingStatus is true (i.e., the book has been read)
+    if (library[i].readingStatus === true) {
+      // Increment the count if the book has been read
+      count++;
+    }
+  }
+
+  // Return the count of books that have been read
+  return count;
+};
+const numberOfReadBooks = numberOfBooksRead(library);
+console.log(numberOfReadBooks);
 // Do not change the code below
 
 alert(numberOfBooksRead());
